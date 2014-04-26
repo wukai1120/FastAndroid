d5bca32dabc6f2f7134cc17ae6b601e44e2aec95
app_debe405e <app_debe405e@ip-10-239-3-116.ec2.internal>
2014-04-26 09:50:43 +0000

    AndroidKickstartR generation

---
+++ FastAndroid/src/com/bobinfo/fastandroid/MainActivity.java
@@ -0,0 +1,25 @@
+
+package com.bobinfo.fastandroid;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity
+    extends SherlockFragmentActivity
+{
+
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
